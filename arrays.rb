c4d81c26fe05c2af7347384700d859c170c9e218
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-05-17 21:21:38 -0700

    Add arrays

---
+++ arrays.rb
@@ -0,0 +1,8 @@
+cities = ["Denver", "Los Angeles", "San Francisco", "Arcata"]
+
+whole_numbers = [1, 2, 3, 4]
+
+bank_balances = [32.33, 43.44, 54.44, 65.55]
+
+online = [true, false, true, false]
+
