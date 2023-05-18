8ff1e9cabcc4e7854bb71be85b004f0acfa525cc
Tommy Takahashi <ttakahashi1591@gmail.com>
2023-05-17 21:44:59 -0700

    Add methods

--- arrays.rb
+++ arrays.rb
@@ -1,8 +1,21 @@
 cities = ["Denver", "Los Angeles", "San Francisco", "Arcata"]
+# The line of code below calls for the `pop` method on the `cities` Array. This method removes and prints the last element in the Array
+# And returns the remaining elements to the Array.
+puts cities.pop
 
 whole_numbers = [1, 2, 3, 4]
+# The line of code below calls for the 'push' method on the `whole_numbers` Array. This method will print the elements listed in the Array
+# `whole_numbers` which we are calling for. 
+puts whole_numbers.push
 
 bank_balances = [32.33, 43.44, 54.44, 65.55]
+# The line of code below calls for the `shift` method on the `bank_balances` Array. This method removes the first element of the Array and
+# Returns the reamining elements to the Array. 
+puts bank_balances.shift
+
 
 online = [true, false, true, false]
+# The line of code below calls for the `unshift` method for the `online` Array. This method adds like given object, `true` in this case, 
+# To the Array in front of the element in the index position 0 and prints the new Array. 
+puts online.unshift true
 
